3b060e1d1aefadef39757cccdfa1b4c840fc12c5
qinlinsen <abrahamlincoln@aliyun.com>
2020-03-28 09:57:55 +0800

    create sample

---
+++ src/main/java/org/genius/common/CommonSample.java
@@ -0,0 +1,8 @@
+package org.genius.common;
+
+/**
+ * this class does nothing it is only use an example
+ * @author qinlinsen
+ */
+public class CommonSample {
+}
---
+++ src/main/java/org/genius/math/MathTest.java
@@ -0,0 +1,9 @@
+package org.genius.math;
+
+/**
+ * this class does nothing it is only use an example
+ * @author qinlinsen
+ */
+public class MathTest {
+
+}
